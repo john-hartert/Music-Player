f205bf27a5c725f053fb03f02ef0666d6dedbd8b
John Hartert <j.blainehartert@gmail.com>
2017-10-25 12:20:26 -0400

    Adds validation functions

--- includes/handlers/register-handler.php
+++ includes/handlers/register-handler.php
@@ -18,25 +18,42 @@ function sanitizeFormString($inputText) {
     return $inputText;
 }
 
+function validateUsername($un) {
+
+}
+
+function validateFirstname($fn) {
+    
+}
+
+function validateLastname($ln) {
+    
+}
+
+function validateEmails($em, $em2) {
+    
+}
+
+function validatePasswords($pw, $pw2) {
+    
+}
+
 if(isset($_POST['registerButton'])){
     //Register button was pressed
     $username = sanitizeFormUsername($_POST['username']);
-
     $firstName = sanitizeFormString($_POST['firstName']);
-    
     $lastName = sanitizeFormString($_POST['lastName']);
-    
     $email = sanitizeFormString($_POST['email']);
-    
     $email2 = sanitizeFormString($_POST['email2']);
-
     $password = sanitizeFormPassword($_POST['password']);
-
     $password2 = sanitizeFormPassword($_POST['password2']);
 
 
-
-
+    validateUsername($username);
+    validateFirstname($firstName);
+    validateLastname($lastName);
+    validateEmails($email, $email2);
+    validatePasswords($password, $password2);
 
 }  
 ?>
