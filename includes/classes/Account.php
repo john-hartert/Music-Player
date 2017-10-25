73f9db637331dafbca50162aeaf76089f1559c0d
John Hartert <j.blainehartert@gmail.com>
2017-10-25 15:54:14 -0400

    Test2

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -59,31 +59,37 @@
                 array_push($this->errorArray, "Your emails don't match");
                 return;
             }
-            if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
-                array_push($this->errorArray, "Email is invalid");
-                return;
-            }
 
-            //To Do: Check that username hasn't already been used.
-        }
-            
-        private function validatePasswords($pw, $pw2) {
-            //This works
-            if($pw != $pw2) {
-                array_push($this->errorArray, "Your passwords don't match");
-                return;
-            }   
-            
-            if(preg_match('/[^A-Za-z0-9]/', $pw)) {
-                array_push($this->errorArray, "Your password can only contain numbers and letters.");
-                return;
-            }
-            if(strlen($pw) > 30 || strlen($pw) < 5) {
-                array_push($this->errorArray, "Your password must be between 5 and 30 characters.");
-                return;
-            }
-            
-        }
-    }
+			if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
+				array_push($this->errorArray, "Email is invalid");
+				return;
+			}
+
+			//TODO: Check that username hasn't already been used
+
+		}
+
+		private function validatePasswords($pw, $pw2) {
+			
+			if($pw != $pw2) {
+				array_push($this->errorArray, "Your passwords don't match");
+				return;
+			}
+
+			if(preg_match('/[^A-Za-z0-9]/', $pw)) {
+				array_push($this->errorArray, "Your password can only contain numbers and letters");
+				return;
+			}
+
+			if(strlen($pw) > 30 || strlen($pw) < 5) {
+				array_push($this->errorArray, "Your password must be between 5 and 30 characters");
+				return;
+			}
+
+		}
+
+
+	}
 ?>
+
         
