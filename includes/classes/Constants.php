fc3a2adc67b8e54cc887c01a0d04256b3087a1bc
John Hartert <j.blainehartert@gmail.com>
2017-10-31 22:20:56 -0500

    Adds more classes

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -2,5 +2,14 @@
 class Constants {
 
     public static $passwordsDoNotMatch = "Your passwords don't match";
+    public static $passwordsNotAlphanumeric = "Your password can only contain numbers and letters";
+    public static $passwordCharacters = "Your password must be between 5 and 30 characters";
+    public static $passwordsDoNotMatch = "Your passwords don't match";
+    public static $passwordsDoNotMatch = "Your passwords don't match";
+    public static $passwordsDoNotMatch = "Your passwords don't match";
+    public static $passwordsDoNotMatch = "Your passwords don't match";
+    public static $passwordsDoNotMatch = "Your passwords don't match";
+    public static $passwordsDoNotMatch = "Your passwords don't match";
+
 }
 ?>
