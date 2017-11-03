98e9ef028711bd5310f7d91b4b42071acff2ca9a
John Hartert <j.blainehartert@gmail.com>
2017-11-03 17:38:54 -0500

    Fixes bug, allows user login by checking if password and username matches in db, and successfully renders error message if username or password is incorrect

--- includes/handlers/login-handler.php
+++ includes/handlers/login-handler.php
@@ -4,7 +4,10 @@ if(isset($_POST['loginButton'])){
     $username = $_POST['loginUsername'];
     $password = $_POST['loginPassword'];
 
-
+    $result = $account->login($username, $password);
+    if($result == true) {
+        header("Location: index.php");
+    }
     //Login function
 }
 ?>
