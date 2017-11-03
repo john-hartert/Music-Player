047ce3f8de02cebc56c346364607c4ee93e854a1
John Hartert <j.blainehartert@gmail.com>
2017-11-03 16:21:03 -0500

    Connects to mysqli db

--- includes/config.php
+++ includes/config.php
@@ -2,4 +2,10 @@
     ob_start();
     $timezone = date_default_timezone_set("America/Chicago");
 
+    $con = mysqli_connect("localhost", "root", "", "clone");
+
+    if(mysqli_connect_errno()) {
+        echo "Failed to connect: " . mysqli_connect_errno();
+    }
+
 ?>
