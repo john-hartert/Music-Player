ef5096318279ab146c37c2654de4e6b2e46c234b
John Hartert <j.blainehartert@gmail.com>
2017-11-04 14:36:32 -0500

    Adds css file and links to it

--- register.php
+++ register.php
@@ -21,6 +21,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome to Music Player</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/register.css">
 </head>
 <body>
     <div id="inputContainer">
