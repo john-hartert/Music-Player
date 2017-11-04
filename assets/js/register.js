a803058b8a48fd4708c34079a49bacdbaebfcafe
John Hartert <j.blainehartert@gmail.com>
2017-11-04 18:26:41 -0500

    Fixes bugs

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,11 +1,11 @@
+
 $(document).ready(function() {
     $("#hideLogin").click(function() {
-        console.log("asdfasdf");
         $("#loginForm").hide();
         $("#registerForm").show();
     });
 
-    $("hideRegister").click(function() {
+    $("#hideRegister").click(function() {
         $("#loginForm").show();
         $("#registerForm").hide();
     });
