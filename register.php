e38ab4200fc41fb6610b495fe80688ef3eb1b711
John Hartert <j.blainehartert@gmail.com>
2017-11-04 17:51:53 -0500

    Trying to debug click function

--- register.php
+++ register.php
@@ -17,12 +17,14 @@
 
 <html lang="en">
 <head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>   
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome to Music Player</title>
     <link rel="stylesheet" type="text/css" href="assets/css/register.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    
+    <script src="assets/js/register.js"></script>
 </head>
 <body>
     <div id="background">
