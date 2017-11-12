cf89ffc42dda70fb66d81acc61784f242cd17daa
John Hartert <j.blainehartert@gmail.com>
2017-11-12 00:20:41 -0600

    Last steps for login page

--- register.php
+++ register.php
@@ -64,7 +64,7 @@
                     <p>
                         <?php echo $account->getError(Constants::$loginFailed); ?>
                         <label for="loginUsername">Username</label>
-                        <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. John Smith" required>
+                        <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. John Smith" value="<?php getInputValue('loginUsername') ?>" required>
                     </p>
                     <p>
                         <label for="loginPassword">Password</label>
