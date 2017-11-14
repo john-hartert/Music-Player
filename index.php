f0652787d49ab4e138646dde90f897e8c64982be
John Hartert <j.blainehartert@gmail.com>
2017-11-14 13:14:01 -0600

    Fixes bug

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 include("includes/config.php");
-//LOGOUT
-// session_destroy();
+
+//session_destroy(); LOGOUT
 
 if(isset($_SESSION['userLoggedIn'])) {
-	$userLoggedin = $_SESSION['userLoggedIn'];
+	$userLoggedIn = $_SESSION['userLoggedIn'];
 }
 else {
 	header("Location: register.php");
@@ -12,97 +12,121 @@ else {
 
 ?>
 
-<html lang="en">
+<html>
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Welcome to Music player!</title>
+
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-	<title>Welcome to Music Player</title>
 </head>
+
 <body>
+	
 
 	<div id="nowPlayingBarContainer">
+
 		<div id="nowPlayingBar">
-			
+
 			<div id="nowPlayingLeft">
 				<div class="content">
 					<span class="albumLink">
-						<img src="https://lh3.googleusercontent.com/dB3Dvgf3VIglusoGJAfpNUAANhTXW8K9mvIsiIPkhJUAbAKGKJcEMPTf0mkSexzLM5o=w300" class="albumArtwork">
+						<img src="https://i.ytimg.com/vi/rb8Y38eilRM/maxresdefault.jpg" class="albumArtwork">
 					</span>
 
 					<div class="trackInfo">
-						
+
 						<span class="trackName">
-							<span>First</span>
+							<span>asdasdfafds</span>
 						</span>
 
 						<span class="artistName">
-							<span>Johnnyboy</span>
+							<span>John Hartert</span>
 						</span>
 
 					</div>
+
+
 
 				</div>
 			</div>
 
 			<div id="nowPlayingCenter">
+
 				<div class="content playerControls">
 
 					<div class="buttons">
+
 						<button class="controlButton shuffle" title="Shuffle button">
 							<img src="assets/images/icons/shuffle.png" alt="Shuffle">
 						</button>
-					
 
-					
 						<button class="controlButton previous" title="Previous button">
 							<img src="assets/images/icons/previous.png" alt="Previous">
 						</button>
-					
 
-					
 						<button class="controlButton play" title="Play button">
 							<img src="assets/images/icons/play.png" alt="Play">
 						</button>
 
-						<button class="controlButton pause" title="Pause button" style="display: none";>
+						<button class="controlButton pause" title="Pause button" style="display: none;">
 							<img src="assets/images/icons/pause.png" alt="Pause">
 						</button>
-					
 
-					
 						<button class="controlButton next" title="Next button">
 							<img src="assets/images/icons/next.png" alt="Next">
 						</button>
-					
 
-					
 						<button class="controlButton repeat" title="Repeat button">
 							<img src="assets/images/icons/repeat.png" alt="Repeat">
 						</button>
+
 					</div>
 
+
 					<div class="playbackBar">
+
 						<span class="progressTime current">0.00</span>
+
 						<div class="progressBar">
 							<div class="progressBarBg">
 								<div class="progress"></div>
+							</div>
 						</div>
+
 						<span class="progressTime remaining">0.00</span>
+
+
 					</div>
 
 
 				</div>
+
+
 			</div>
 
 			<div id="nowPlayingRight">
+				<div class="volumeBar">
+
+					<button class="controlButton volume" title="Volume button">
+						<img src="assets/images/icons/volume.png" alt="Volume">
+					</button>
+
+					<div class="progressBar">
+						<div class="progressBarBg">
+							<div class="progress"></div>
+						</div>
+					</div>
+
+				</div>
 			</div>
 
 
+
+
 		</div>
+
 	</div>
 
 
 </body>
+
 </html>
